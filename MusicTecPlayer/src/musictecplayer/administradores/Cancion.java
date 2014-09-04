b90c10f78652d2da0dc1ef45c3b5fb842995d8f9
Joseph <j-vv01@hotmail.com>
2014-09-04 16:27:32 -0600

    Documentación completa Cancion.java

--- MusicTecPlayer/src/musictecplayer/administradores/Cancion.java
+++ MusicTecPlayer/src/musictecplayer/administradores/Cancion.java
@@ -1,18 +1,19 @@
-package musictecplayer.administradores;
-
 /**
  * @author Lucia Solis Ceciliano  
  * @author Joseph Vega Vargas
  * @author Miller Ruiz Urbina
- * 
- * Clase que va a almacenar los datos de las canciones y va a permitir realizar
- * todos los procesos necesarios en las canciones
+ */
+
+package musictecplayer.administradores; //Paquete Netbeans 8.0
+
+/**
+ * Clase que contiene todos los atributos de una canción, y mediante gets y sets
+ * se puede editar un atributo.
  */
 public class Cancion {
     /**
      * Variables Globales:
-     * @var nombre, artista, genero, album, año, rutaCancion, rutaImagen: 
-     * controlan los atributos de las canciones
+     * Controlan los atributos de una canción.
      */
     private String nombre;
     private String artista;
@@ -23,7 +24,7 @@ public class Cancion {
     private String rutaImagen;
 
     /**
-     * Método constructor de la clase
+     * Método constructor de la clase.
      * 
      * @param nombre
      * @param artista
@@ -33,7 +34,8 @@ public class Cancion {
      * @param rutaCancion
      * @param rutaImagen 
      */
-    public Cancion(String nombre, String artista, String genero, String album, String año, String rutaCancion, String rutaImagen) {
+    public Cancion(String nombre, String artista, String genero, String album, 
+            String año, String rutaCancion, String rutaImagen) {
         this.nombre = nombre;
         this.artista = artista;
         this.genero = genero;
@@ -42,68 +44,134 @@ public class Cancion {
         this.rutaCancion = rutaCancion;
         this.rutaImagen = rutaImagen;
     }
-
+    
     /**
-     * Metodos Get permiten obtener el datos solicitado de las variables
-     * Metodos Set permiten modificar los datos indirectamente a la variable
-     * @return String || int
+     * Devuelve el nombre de la canción.
+     * @return nombre
      */
     public String getNombre() {
         return nombre;
     }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    
+    /**
+     * Devuelve el artista de la canción.
+     * @return artista
+     */
     public String getArtista() {
         return artista;
     }
-    public void setArtista(String artista) {
-        this.artista = artista;
-    }
+    
+    /**
+     * Devuelve el género de la canción.
+     * @return genero
+     */
     public String getGenero() {
         return genero;
     }
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
+    
+    /**
+     * Devuelve el álbum de la canción.
+     * @return album
+     */
     public String getAlbum() {
         return album;
     }
-    public void setAlbum(String album) {
-        this.album = album;
-    }
+    
+    /**
+     * Devuelve el año de la canción.
+     * @return año
+     */
     public String getAño() {
         return año;
     }
-    public void setAño(String año) {
-        this.año = año;
-    }
+    
+    /**
+     * Devuelve la ruta de la canción.
+     * @return rutaCancion
+     */
     public String getRutaCancion() {
         return rutaCancion;
     }
+    
+    /**
+     * Devuelve la ruta de la imagen de la canción.
+     * @return rutaImagen
+     */
     public String getRutaImagen() {
         return rutaImagen;
     }
+    
+    /**
+     * Edita indirectamente el nombre de la canción.
+     * @param nombre 
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /**
+     * Edita indirectamente el artista de la canción.
+     * @param artista 
+     */
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+    
+    /**
+     * Edita indirectamente el género de la canción.
+     * @param genero 
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    /**
+     * Edita indirectamente el álbum de la canción.
+     * @param album 
+     */
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+    
+    /**
+     * Edita indirectamente el año de la canción.
+     * @param año 
+     */
+    public void setAño(String año) {
+        this.año = año;
+    }
+    
+    /**
+     * Edita indirectamente la ruta de la canción.
+     * @param rutaCancion 
+     */
     public void setRutaCancion(String rutaCancion) {
         this.rutaCancion = rutaCancion;
     }
+    
+    /**
+     * Edita indirectamente la ruta de la imagen de la canción.
+     * @param rutaImagen 
+     */
     public void setRutaImagen(String rutaImagen) {
         this.rutaImagen = rutaImagen;
     }
 
     /**
-     * Método que imprime los datos para la comprobación del almacenaje
-     * @return 
+     * Imprime todos los atributos de la canción.
+     * @return resultado
      */
     @Override
     public String toString() {
-        String resultado = "";
-        resultado += "Nodo:\n"
-                + nombre + "\n"
-                + artista + "\n"
-                + album + "\n"
-                + año + "\n"
-                + rutaCancion + "\n";
+        String resultado = "Nodo:"
+                + "\n" + nombre 
+                + "\n" + artista 
+                + "\n" + album 
+                + "\n" + genero 
+                + "\n" + año 
+                + "\n" + rutaCancion 
+                + "\n" + rutaImagen + "\n";
+        
         return resultado;
     }
 }
